e18da7c7cd9484071a99aba92de4e887c35a8078
elleestcrimi <iistcrimi@gmail.com>
2011-12-16 22:42:10 +0200

    Adjusted the animations to be smoother

--- jquery.multipleDraggables.js
+++ jquery.multipleDraggables.js
@@ -1,88 +1,108 @@
 /*
-
-A jQuery multiple draggable
-
-Version 0.3
-
-Author:
-	Hadeer Younis
-
-Project ho'.ui-multi-draggables':
-	http://elleestcrimi.'.ui-multi-draggables'/2011/12/16/jquery-multiple-draggables
-
-License:
-This source file is subject to the BSD license bundled with this package.
-Available online: {@link http://www.opensource.org/licenses/bsd-license.php}
-If you did not receive a copy of the license, and are unable to obtain it, 
-learn to use a search engine.
-*/
+ * jQuery Multiple Draggables
+ *
+ * Version 0.4
+ *
+ * Author : Hadeer Younis <iistcrimi@gmail.com>
+ *
+ */
 (function ($) {
     $.fn.multipleDraggable = function (options) {
         var settings = $.extend({
-            duration: 300,
-            start:function(){},
-            drag:function(){},
-            stop:function(){},
+            duration: 1000,
+            start: function () {},
+            drag: function () {},
+            stop: function () {},
         }, options);
         var no_animate = false;
+        var offset = 0;
+        var toggle = true;
         this.addClass('ui-multi-draggables');
+        this.mousedown(function () {
+            $(this).addClass('ui-multi-dragging');
+        })
         attachtaskDragEventHandler();
-        
 
-        /*******************HELPER '.ui-multi-draggables'THODS***************************/
+        /*******************HELPER METHODS***************************/
+
         function attachtaskDragEventHandler() {
             $('.ui-multi-draggables').draggable({
                 start: function () {
-                    $('.ui-multi-draggables').each(function () {
-                        $(this).attr('x', $(this).offset().left).attr('y', $(this).offset().top).css({
-                            top: $(this).attr('y') + 'px',
-                            left: $(this).attr('x') + 'px'
-                        })
-                    }).css('position', 'absolute');
+                    startHelper();
                     settings.start();
                 },
-                stop: function () {
-                    taskDragStopHelper();
-                },
                 drag: function (e) {
-                    taskDragDraggingHelper(e);
+                    dragHelper(e);
                     settings.drag();
+                },
+                stop: function () {
+                    stopHelper();
                 }
             }).removeAttr('style')
         }
 
-        function taskDragStopHelper() {
+        function startHelper() {
+
+            offset = $('.ui-multi-dragging:first').offset();
+            $('.ui-multi-draggables').each(function () {
+                $(this).attr('x', $(this).offset().left).attr('y', $(this).offset().top).css({
+                    top: $(this).attr('y') + 'px',
+                    left: $(this).attr('x') + 'px'
+                })
+            }).css('position', 'absolute');
+            window.setTimeout(help, 200);
+        }
+
+        function stopHelper() {
             $('.ui-multi-draggables').each(function () {
                 $(this).animate({
                     top: $(this).attr('y') + 'px',
                     left: $(this).attr('x') + 'px'
                 }, settings.duration, function () {
                     $(this).removeAttr('style');
-                    wait = 0;
                     settings.stop();
                 });
             });
             no_animate = false;
         }
 
-        function taskDragDraggingHelper(e) {
-            if (!no_animate) {
+        function help() {
+            toggle = false;
+            $('.ui-multi-draggables').each(function () {
+                var offset_me = $('.ui-multi-dragging:first').offset();
+                $(this).stop().animate({
+                    top: offset_me.top + 'px',
+                    left: offset_me.left + 'px'
+                }, 100, function () {
+                    no_animate = true;
+                    toggle = true;
+                });
+            });
+        }
+
+        function dragHelper(e) {
+            var offset_new = $('.ui-multi-dragging:first').offset();
+            if (!no_animate && toggle) {
                 $('.ui-multi-draggables').each(function () {
-                    var x = e.pageX - $(this).width() / 2;
-                    var y = e.pageY - $(this).height() / 2;
-                    if (y - 5 < $(this).offset().top && y + 5 > $(this).offset().top && x - 5 < $(this).offset().left && x + 5 > $(this).offset().left) {
-                        no_animate = true;
-                    }
+                    var me_x = parseInt($(this).attr('x'), 10) + (offset_new.left - offset.left);
+                    var me_y = parseInt($(this).attr('y'), 10) + (offset_new.top - offset.top);
+                    $(this).css({
+                        top: me_y + 'px',
+                        left: me_x + 'px'
+                    });
+                });
+            } else if (!toggle) {
+                $('.ui-multi-draggables').each(function () {
                     $(this).stop().animate({
-                        top: y + 'px',
-                        left: x + 'px'
-                    }, settings.duration);
+                        top: offset_new.top + 'px',
+                        left: offset_new.top + 'px'
+                    }, 100, function () {
+                        no_animate = true;
+                        toggle = true;
+                    });
                 });
             } else {
-                $('.ui-multi-draggables').css({
-                    top: e.pageY - $(this).width() / 2 + 'px',
-                    left: e.pageX - $(this).height() / 2 + 'px'
-                });
+                $('.ui-multi-draggables').offset(offset_new);
             };
         }
     };
