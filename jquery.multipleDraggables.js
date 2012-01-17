e5c1b0eb938bf857f6ec8c9154ae813cca68b804
elleestcrimi <iistcrimi@gmail.com>
2012-01-18 00:43:58 +0200

    Enabled the item selection feature, updated the README and demo page

--- jquery.multipleDraggables.js
+++ jquery.multipleDraggables.js
@@ -1,29 +1,73 @@
 /*
  * jQuery Multiple Draggables
  *
- * Version 0.5
+ * Version 0.6
  *
  * Author : Hadeer Younis <iistcrimi@gmail.com>
  *
  */
 (function ($) {
-    $.fn.multipleDraggable = function (options) {
-        var settings = $.extend({
-            start: function () {},
-            drag: function () {},
-            stop: function () {}
-        }, options);
-        var offset = 0;
-        this.addClass('ui-multi-draggables');
-        this.mousedown(function () {
-            $(this).addClass('ui-multi-dragging');
-        })
-        attachtaskDragEventHandler();
+    $.fn.multipleDraggables = function (options) {
 
+        var settings = $.extend({
+            start: function() {},
+            drag: function() {},
+            stop: function() {},
+            select: false,
+            click: function() {},
+            placeholder: 'clone',
+            highlight: 'highlight'
+        }, options);
+
+        var offset = 0;
+        var us = this;
+        var t;
+        var to_drag = this;
+
+        if(settings.select)
+        {
+            onClickHandler();
+        }
+        else
+        {
+            this.mousedown(function () {
+                $('.ui-multi-dragging').removeClass('ui-multi-dragging');
+                $(this).addClass('ui-multi-dragging');
+            });
+            attachDragEventHandler(this);
+        }
         /*******************HELPER METHODS***************************/
 
-        function attachtaskDragEventHandler() {
-            $('.ui-multi-draggables').draggable({
+        function onClickHandler()
+        {
+            $(us).click(function(){
+                if($(this).hasClass('ui-multi-fix'))
+                {
+                    $(this).removeClass('ui-multi-fix');
+                    return;
+                }
+                $(this).toggleClass('ui-multi-selected '+settings.highlight);
+                if($(this).hasClass('ui-multi-selected '+settings.highlight))
+                {
+                    to_drag = $('.ui-multi-selected');
+                    $(this).mousedown(function () {
+                        $('.ui-multi-dragging').removeClass('ui-multi-dragging');
+                        $(this).addClass('ui-multi-dragging');
+                    });
+                    attachDragEventHandler(this);
+                }
+                else
+                {
+                    to_drag = $('.ui-multi-selected');
+                    $(this).draggable("destroy")
+                    $(this).unbind('mousedown');
+                }
+                settings.click()
+            });
+        }
+
+        function attachDragEventHandler(me) {
+            $(me).draggable({
                 start: function () {
                     startHelper();
                     settings.start();
@@ -32,44 +76,71 @@
                     dragHelper(e);
                     settings.drag();
                 },
+                addClasses: false,
                 stop: function () {
-                    stopHelper();
-                }
+                    stopHelper();  
+                },
+                revert: function(){
+                    revertHelper();
+                    return true;
+                },
+                revertDuration: 500,
+                zIndex:10
             }).removeAttr('style')
         }
 
         function startHelper() {
-            offset = $('.ui-multi-dragging:first').offset();
-            $('.ui-multi-draggables').each(function () {
-                $(this).attr('x', $(this).offset().left).attr('y', $(this).offset().top).offset($(this).offset());
+            offset = $('.ui-multi-dragging').position();
+            $('.ui-multi-dragging').addClass('ui-multi-fix')
+            var i = 0;
+            var sisters = [];
+            $(to_drag).each(function () {
+                jQuery.data(this, 'x', $(this).position().left);
+                jQuery.data(this, 'y', $(this).position().top);
+                sisters.push($(this).clone())
             }).css('position', 'absolute');
-            window.setTimeout(help, 400);
+            $(to_drag).each(function(){
+                $(this).after(sisters[i].addClass(settings.placeholder));
+                if($(this).hasClass('ui-multi-dragging'))
+                {
+                    $(this).css('margin-right','-'+$(this).outerWidth()+'px')
+                    $(this).next().removeClass('ui-multi-dragging');
+                }
+                i++;
+            });
+            t = window.setTimeout(help, 400);
         }
 
-        function stopHelper() {
-            $('.ui-multi-draggables').addClass('animate2').each(function () {
-                $(this).css({
+        function revertHelper() {
+            clearTimeout(t);
+            $(to_drag).each(function () {
+                if($(this).hasClass('ui-multi-dragging'))
+                    return;
+                $(this).addClass('animate2').css({
                     transform: 'rotate(0deg)',
                     '-moz-transform': 'rotate(0deg)',
                     '-o-transform': 'rotate(0deg)',
                     '-webkit-transform': 'rotate(0deg)',
-                    top: $(this).attr('y') + 'px',
-                    left: $(this).attr('x') + 'px'
+                    top: jQuery.data(this, "y") + 'px',
+                    left: jQuery.data(this, "x") + 'px'
                 })
             })
-            window.setTimeout(cleanup, 700);
         }
 
-        function cleanup() {
-            $('.ui-multi-draggables,.ui-multi-dragging').removeClass('ui-multi-dragging rotate animate animate2').removeAttr('style');
+        function stopHelper() {
+            $(to_drag).removeClass('rotate animate animate2').removeAttr('style')
+            $('.clone').remove()
             settings.stop();
         }
 
         function help() {
             var deg = 15;
-            $('.ui-multi-draggables:not([class~="ui-multi-dragging"])').addClass('animate').each(function () {
-                var d_x = offset.left - parseInt($(this).attr('x'));
-                var d_y = offset.top - parseInt($(this).attr('y'));
+            $(to_drag).each(function () {
+                if($(this).hasClass('ui-multi-dragging'))
+                    return;
+                $(this).addClass('animate')
+                var d_x = offset.left - parseInt(jQuery.data(this, "x"));
+                var d_y = offset.top - parseInt(jQuery.data(this, "y"));
                 deg *= -1;
                 $(this).css({
                     transform: 'translate(' + d_x + 'px,' + d_y + 'px) rotate('+deg+'deg) ',
@@ -81,10 +152,12 @@
         }
 
         function dragHelper(e) {
-            var offset_new = $('.ui-multi-dragging:first').offset();
-            $('.ui-multi-draggables').each(function () {
-                var me_x = parseInt($(this).attr('x'), 10) + (offset_new.left - offset.left);
-                var me_y = parseInt($(this).attr('y'), 10) + (offset_new.top - offset.top);
+            var offset_new = $('.ui-multi-dragging').position();
+            $(to_drag).each(function () {
+                if($(this).hasClass('ui-multi-dragging'))
+                    return;
+                var me_x = parseInt(jQuery.data(this, "x"), 10) + (offset_new.left - offset.left);
+                var me_y = parseInt(jQuery.data(this, "y"), 10) + (offset_new.top - offset.top);
                 $(this).css({
                     top: me_y + 'px',
                     left: me_x + 'px'
